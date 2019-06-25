dcd1844d7464dd04ff92972995dde92055277d54
Julia <julyettka@gmail.com>
2019-06-25 09:08:47 +0300

    rewrite tests with axios-mock-adapter

--- tests/unit/textBox.spec.js
+++ tests/unit/textBox.spec.js
@@ -2,54 +2,52 @@ import { mount } from '@vue/test-utils';
 import TextBox from '@/components/TextBox.vue';
 import axios from 'axios';
 import BASEURL from '@/api';
+import MockAdapter from 'axios-mock-adapter';
 
-jest.mock('axios', () => ({
-  post: jest.fn().mockReturnValue({
-    then: jest.fn().mockReturnValue({
-      data: 'testData',
-      catch: jest.fn().mockReturnValue({ err: 'some error' }),
-    }),
-  }),
-}));
 
 describe('TextBox.vue component', () => {
   let wrapper;
 
-  beforeEach(() => wrapper = mount(TextBox));
+  beforeEach(() => {
+    wrapper = mount(TextBox);
+  });
 
   it('has an input and a text box', () => {
     expect(wrapper.contains('#tb-input')).toBe(true);
     expect(wrapper.contains('#tb-text-area')).toBe(true);
   });
 
-  describe('addTask method', () => {
+  describe('Async actions in TextBox.vue component', () => {
+    let mockAdapter;
     let mockedTaskObj;
 
     beforeEach(() => {
+      mockAdapter = new MockAdapter(axios);
       mockedTaskObj = {
         title: 'some title',
         text: 'some text',
         completed: false,
         archived: false,
       };
-    });
 
-    it('should call axios post method with parameters', () => {
       wrapper.vm.newTaskTitle = mockedTaskObj.title;
       wrapper.vm.newTaskText = mockedTaskObj.text;
-
-      wrapper.vm.addTask();
-      expect.assertions(1);
-      expect(axios.post).toBeCalledWith(`${BASEURL}/tasks`, mockedTaskObj);
     });
 
-    // it('should emit `addTaskEvent` event', () => {
-    //   wrapper.vm.newTaskTitle = mockedTaskObj.title;
-    //   wrapper.vm.newTaskText = mockedTaskObj.text;
-    //   jest.spyOn(axios.post, 'then');
-    //   jest.spyOn(wrapper.vm.$root, '$emit');
-    //
-    //   expect(wrapper.vm.$root.$emit).toBeCalledWith('addTaskEvent', 'testData');
+    // it('Should call add task method', () => {
+    //   wrapper.vm.addTask();
+    //   expect(mockAdapter.onPost).toBeCalledWith(`${BASEURL}/tasks`, mockedTaskObj);
     // });
+
+    it('Should return data from response after post', async () => {
+      mockAdapter.onPost(`${BASEURL}/tasks`, mockedTaskObj).reply(200, mockedTaskObj);
+      const response = wrapper.vm.addTask();
+
+      setTimeout(() => {
+        console.log(response);
+        expect(response.mockedTaskObj.title).toEqual('some title');
+        expect(response.mockedTaskObj.text).toEqual('some text');
+      }, 0);
+    });
   });
 });
