9cbef764e965118e8995b8cb70a34df68e36523b
Julia <julyettka@gmail.com>
2019-07-22 08:35:40 +0300

    upd

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,12 @@ module.exports = {
     s3Deploy: {
       registry: undefined,
       awsProfile: 'default',
-      region: 'eu-central-1',
-      bucket: 'my-keep',
+      region: 'eu-west-3',
+      bucket: 'mykeepbucket',
       createBucket: false,
       staticHosting: true,
       staticIndexPage: 'index.html',
-      staticErrorPage: 'error.html',
+      staticErrorPage: 'index.html',
       assetPath: 'dist',
       assetMatch: '**',
       deployPath: '/',
@@ -17,7 +17,7 @@ module.exports = {
       pwaFiles: 'service-worker.js',
       enableCloudfront: false,
       uploadConcurrency: 5,
-      pluginVersion: '3.0.0'
-    }
-  }
-}
+      pluginVersion: '3.0.0',
+    },
+  },
+};
